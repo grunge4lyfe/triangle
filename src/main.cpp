c103582011e4a2d1700f25fd9251e56d5b548f0a
DelightedCat <git@delightedcat.net>
2022-07-27 09:58:09 +0200

    Improve error handling and rename variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <stdexcept>
+
 #include "glad/gl.h"
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 
-const char *vertex_source = "#version 330 core\n"
+const char *vertex_shader_source = "#version 330 core\n"
     "layout (location = 0) in vec2 aPos;\n"
     "layout (location = 1) in vec3 aColor;\n"
     "out vec3 oColor;\n"
@@ -12,7 +14,7 @@ const char *vertex_source = "#version 330 core\n"
     "    oColor = aColor;\n"
     "}\0";
 
-const char *fragment_source = "#version 330 core\n"
+const char *fragment_shader_source = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "in vec3 oColor;\n"
     "void main() {\n"
@@ -25,6 +27,9 @@ static float vertices[] = {
     0.0f, 0.5f, 0.0f, 0.0f, 1.0f
 };
 
+static int success;
+static char info_log[512];
+
 static unsigned int VBO;
 static unsigned int VAO;
 
@@ -34,48 +39,60 @@ static unsigned int fragment_shader;
 static unsigned int shader_program;
 
 void glfw_error_callback(int error, const char *description) {
-    fprintf(stderr, "GLFW error (%d): %s\n", error, description);
+    throw std::runtime_error("GLFW error " + std::to_string(error) + ": " + description);
 }
 
-void glfw_key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-int main(int argc, char *argv[]) {
+int main(void) {
     glfwSetErrorCallback(glfw_error_callback);
 
     if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW!" << std::endl;
-        return -1;
+        throw std::runtime_error("Failed to initialize GLFW!");
     }
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello, world!", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window!" << std::endl;
-        return -1;
+    GLFWwindow *handle = glfwCreateWindow(800, 600, "Hello, triangle!", nullptr, nullptr);
+    if (!handle) {
+        throw std::runtime_error("Failed to create GLFW window!");
     }
 
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, glfw_key_callback);
+    glfwMakeContextCurrent(handle);
 
-    if (gladLoadGL(glfwGetProcAddress) == 0) {
-        std::cerr << "Failed to initialize GLAD!" << std::endl;
-        return -1;
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        throw std::runtime_error("Failed to initialize GLAD!");
     }
 
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_source, nullptr);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
     glCompileShader(vertex_shader);
 
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, sizeof(info_log), nullptr, info_log);
+        throw std::runtime_error("Vertex shader compile error: " + std::string(info_log));
+    }
+
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_source, nullptr);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
     glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, sizeof(info_log), nullptr, info_log);
+        throw std::runtime_error("Fragment shader compile error: " + std::string(info_log));
+    }
 
     shader_program = glCreateProgram();
     glAttachShader(shader_program, vertex_shader);
     glAttachShader(shader_program, fragment_shader);
     glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        glGetProgramInfoLog(shader_program, sizeof(info_log), nullptr, info_log);
+        throw std::runtime_error("Program link error: " + std::string(info_log));
+    }
 
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
@@ -95,9 +112,7 @@ int main(int argc, char *argv[]) {
 
     glBindVertexArray(0);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
+    while (!glfwWindowShouldClose(handle)) {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -106,10 +121,11 @@ int main(int argc, char *argv[]) {
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(handle);
+        glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    glfwDestroyWindow(handle);
     glfwTerminate();
 
     return 0;
